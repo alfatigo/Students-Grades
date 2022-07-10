a8e389b7c1db51d1bc09515655cf19e3821fe04c
alfatigo <alfatigo@users.noreply.github.com>
2022-07-10 00:18:03 -0400

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,22 +1,8 @@
-let students = [];
 let form = document.querySelector("form");
+let students = [];
 const avrgBtn = document.querySelector("#AvrgCalculation");
 window.addEventListener("load", () => {
-
-
-  let status = false;
   const addStudent = document.querySelector("#addStudent");
-
-
-  //Get the table
-  const table = document.querySelector("table");
-  //set where you wanna insert the row in the table
-  let row = table.insertRow(1);
-  //position for data in the table
-  let tName = row.insertCell(0);
-  let tLastname = row.insertCell(1);
-  let tId = row.insertCell(2);
-  let tScore = row.insertCell(3);
 
   //constructor for students this initialize
   class Students {
@@ -27,46 +13,68 @@ window.addEventListener("load", () => {
       this.score = score;
     }
   }
+  function inputValidator(firstName, lastName, id, score) {
+    if (firstName != "" && lastName != "" && id != "" && score != "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  addStudent.addEventListener("click", (e) => {
+    e.preventDefault();
+  
 
 
-
-  addStudent.addEventListener('click', (e) => {
     const FirstName = document.getElementById("name").value;
     const LastName = document.getElementById("lastName").value;
     const Id = document.getElementById("Id").value;
     const Score = document.getElementById("score").value;
     let student = new Students(FirstName, LastName, Id, Score);
-    e.preventDefault()
-    if (
-      student.name != " " ||
-      student.lname != " " ||
-      student.id != " " ||
-      student.score != " "
+    if (inputValidator(FirstName, lastName, Id, Score)) {
+      //Get the table
+      const table = document.querySelector("table");
+      //set where you wanna insert the row in the table
+      let row = table.insertRow(1);
+      //position for data in the table
+      let tName = row.insertCell(0);
+      let tLastname = row.insertCell(1);
+      let tId = row.insertCell(2);
+      let tScore = row.insertCell(3);
+      let actionBtn = row.insertCell(4);
 
-    ) {
-      console.log("true")
+      let div = document.createElement('div');
+  let button = document.createElement('button');
+      students.push(student);
+
+      function del(index, arr, val){
+            arr[index].name =val
+      }
+      students.map((items, index) => {
+        tName.textContent = items.name;
+        tLastname.textContent = items.lname;
+        tId.textContent = items.id;
+        tScore.textContent = items.score;
+
+        actionBtn.append(button)
+        button.setAttribute('id', 'btnDel')
+        button.addEventListener('click', () =>  {
+           del(index, students, 'sdfsdfsd');
+          
+         
+        })
+        
+      });
+      form.reset();
     } else {
-      console.log('false')
+      console.log(0);
     }
   });
-
-
-
-
-
-  // students.push(student);
-
-  // tName.innerHTML = student["name"];
-  // tLastname.innerHTML = student["lname"];
-  // tId.innerHTML = student["id"];
-  // tScore.innerHTML = student["score"];
 });
-
 const avrgTxt = document.querySelector("p");
-
 avrgBtn.addEventListener("click", () => {
+  e.preventDefault();
   let avrg = students.reduce((a, b) => {
-    return (a + parseInt(b.score) / students.length);
+    return a + parseInt(b.score) / students.length;
   }, 0);
-  console.log(avrg)
+  console.log(avrg);
 });
