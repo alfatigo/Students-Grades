48a071741c6fbb8c2fdad9ac066ecf51b15ed3ff
alfatigo <alfatigo@users.noreply.github.com>
2022-07-23 11:42:21 -0400

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,11 +1,8 @@
-let form = document.querySelector("form");
 let students = [];
-
+var local = JSON.parse(localStorage.getItem("students"));
+let EditBtn = document.querySelector('#edit')
 window.addEventListener("load", () => {
-  const addStudent = document.querySelector("#addStudent");
-  let local = JSON.parse(localStorage.getItem("students"));
-
-  let table = document.querySelector("table");
+  let addStudent = document.querySelector("#addStudent");
 
   class Students {
     constructor(name, lname, id, score) {
@@ -15,83 +12,121 @@ window.addEventListener("load", () => {
       this.score = score;
     }
   }
-  //#region input validation
-  function inputValidator(firstName, lastName, id, score) {
-    if (firstName != "" && lastName != "" && id != "" && score != "") {
-      return true;
-    } else {
-      alert("Existen campos vacios!");
-      return false;
-    }
+
+  if (localValues(local, students)) {
+    Render();
   }
-  //#endregion
+
   addStudent.addEventListener("click", (e) => {
     e.preventDefault();
-    //#region set input Buttons
-    const FirstName = document.getElementById("name").value.trim();
-    const LastName = document.getElementById("lastName").value.trim();
-    const Id = document.getElementById("Id").value.trim();
-    const Score = document.getElementById("score").value.trim();
-    //#endregion
-    // Create new student
-    let student = new Students(FirstName, LastName, Id, Score);
 
-    if (inputValidator(FirstName, lastName, Id, Score)) {
-      //#region Table Position
-      let row = table.insertRow(1);
-      //position for data in the table
-      let tName = row.insertCell(0);
-      let tLastname = row.insertCell(1);
-      let tId = row.insertCell(2);
-      let tScore = row.insertCell(3);
-      let editBtn = row.insertCell(4);
-      let delBtn = row.insertCell(4);
-      //#endregion
+    const FirstName = document.getElementById("name");
+    const LastName = document.getElementById("lastName");
+    const Id = document.getElementById("Id");
+    const Score = document.getElementById("score");
 
-      //#region add Obj Student to Array
+    let student = new Students(
+      FirstName.value,
+      LastName.value,
+      Id.value,
+      Score.value
+    );
+
+    if (validator(FirstName.value, lastName.value, Id.value, Score.value)) {
       students.push(student);
-      //#endregion
-
-      //#region Add student to tha table
-      students.map((item, index) => {
-        tName.textContent = item.name;
-        tLastname.textContent = item.lname;
-        tId.textContent = item.id;
-        tScore.textContent = item.score;
-        delBtn.innerHTML = `<i class='onDeletebtn material-icons' onClick='onDelete(this, ${index})'>delete</i>`;
-      });
-      //#endregion
-
-      //Clear the input Form
+      Render();
       form.reset();
+      return (local = localStorage.setItem(
+        "students",
+        JSON.stringify(students.flat())
+      ));
     } else {
     }
-
-    localStorage.setItem("students", JSON.stringify(students));
   });
 });
 
-//#region Action Buttons
+function Render() {
+  //#region Table Position
+  let table = document.querySelector("table tbody");
+  results = "";
+  students.flat().forEach((item, index) => {
+    results += `<tr>
+        <td>
+          ${item.name}     
+        </td>
+        <td>      
+          ${item.lname} 
+        </td>  
+        <td>    
+          ${item.id}  
+        </td>  
+        <td>  
+        ${item.score}
+        </td>  
+        <td>  
+          <i class='onDeletebtn material-icons' onClick='onDelete(this, ${index})'>delete</i>
+          <i class='onEditBtn material-icons'onClick='onEdit(this,${index},${item.name},${item.lname},${item.id}, ${item.score})'>create</i>
+          </td>  
+      </tr>`;
+  });
+
+  table.innerHTML = results;
+}
+function validator(firstName, lastName, id, score) {
+  if (firstName != "" && lastName != "" && id != "" && score != "") {
+    {
+      if (score < 100 || score > 0 ) {
+      alert("Error! las notas deben ser mayor que 0 y menor que 100");
+      return false;
+    }
+      return true
+    }
+  }  else {
+    alert("Existen campos vacios!");
+    return false;
+  }
+}
 
 function onDelete(td, index) {
   if (confirm("Are you sure you want to delete this?")) {
     row = td.parentElement.parentElement;
     document.querySelector("table").deleteRow(row.rowIndex);
-    students.splice(index, 1);
+    local.splice(index, 1);
   }
+  return localStorage.setItem("students", JSON.stringify(students.flat()));
 }
 
-//#endregion
+const FirstName = document.getElementById("name");
+const LastName = document.getElementById("lastName");
+const Id = document.getElementById("Id");
+const Score = document.getElementById("score");
 
-//#region Get Average Score
+function onEdit(td, index, name, lname, id, score) {
+  FirstName.value = name;
+  LastName.value  = lname
+  Id.value = id;
+  Score.value = score; 
+  local[0].name = name;
+
+  
+
+}
+function localValues(storageVal, current) {
+  if (storageVal) {
+    console.log(storageVal);
+    current.push(storageVal);
+    return true;
+  } else return false;
+}
 
 const avrgBtn = document.querySelector("#AvrgCalculation");
 const avrgTxt = document.querySelector("output");
+
 avrgBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   let avrg = students.reduce((a, b) => {
-    return a + parseInt(b.score) / students.length;
+    return (a + parseInt(b[0].score) / local.length);
   }, 0);
 
   if (avrg <= 0) alert("El promedio es menor o igual a 0");
